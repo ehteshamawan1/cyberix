452198a7a5139adc1e1cb1710b696352355b9458
ehteshamawan1 <ehtesham.awan.ea@gmail.com>
2024-03-22 23:34:18 +0500

    committed

--- index.js
+++
@@ -1,8 +0,0 @@
-var Typed = new Typed("#typed", {
-    strings: ['design', 'development','marketing', 'security'],
-    typeSpeed: 35,
-    startDelay: 500,
-    backDelay: 1000,
-    backSpeed: 35,
-    fadeOut: true
-});
